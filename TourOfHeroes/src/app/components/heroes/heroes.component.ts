aa8115f618f1cf50cc8086a528718fc6590e7d19
CiprianOlteanu23 <ciprian.olteanu@amdaris.com>
2022-09-17 20:59:06 +0300

    added tutorial project

---
+++ TourOfHeroes/src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/app/hero';
+import { HEROES } from 'src/app/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero?: Hero;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+}
